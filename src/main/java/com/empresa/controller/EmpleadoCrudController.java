2a09818e18b8991e249d543e84ad54e07852b2f0
Alex <abellezap@gmail.com>
2023-10-19 20:02:08 -0500

    ajustes

--- src/main/java/com/empresa/controller/EmpleadoCrudController.java
+++ src/main/java/com/empresa/controller/EmpleadoCrudController.java
@@ -8,14 +8,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.empresa.entity.Empleado;
 import com.empresa.service.EmpleadoService;
-import com.empresa.util.FunctionUtil;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -45,19 +43,6 @@ public class EmpleadoCrudController {
 		obj.setFechaRegistro(new Date());
 		obj.setFechaActualizacion(new Date()); 
 	
-		//Para la PC2
-		//Usuario objUsuario=(Usuario)session.getAttribute("objUsuario");
-		//obj.setUsuarioRegistro(objUsuario);
-		//obj.setUsuarioActualiza(objUsuario);
-		
-		//Validaciones por nombre y apellido
-		List<Empleado> lstSalida = empleadoService.listaPorNombreApellidoIgual(obj.getNombres(), obj.getApellidos());
-		if (!CollectionUtils.isEmpty(lstSalida)) {
-			map.put("mensaje", "El empleado " + obj.getNombres()+ " " + obj.getApellidos() + " ya existe");
-			List<Empleado> lista = empleadoService.listaPorNombreApellidoLike("%");
-			map.put("lista", lista);
-			return map;
-		}
 		
 		Empleado objSalida = empleadoService.insertaEmpleado(obj);
 		if (objSalida == null) {
@@ -72,16 +57,6 @@ public class EmpleadoCrudController {
 		return map;
 	}
 	
-	@GetMapping("/buscaEmpleadoMayorEdad")
-	@ResponseBody
-	public String validaMayorEdad(String fechaNacimiento) {
-		if(FunctionUtil.isMayorEdad(fechaNacimiento)) {
-			return "{\"valid\":true}";
-		}else {
-			return "{\"valid\":false}";
-		}
-	}
-	
 	
 	@PostMapping("/actualizaCrudEmpleado")
 	@ResponseBody
@@ -91,23 +66,10 @@ public class EmpleadoCrudController {
 
 		
 		Optional<Empleado> optEmpleado= empleadoService.buscaEmpleado(obj.getIdEmpleado());
-		
-		//Validaciones por nombre y apellido
-		List<Empleado> lstSalida = empleadoService.listaPorNombreApellidoIgualActualiza(obj.getNombres(), obj.getApellidos(), optEmpleado.get().getIdEmpleado());
-		if (!CollectionUtils.isEmpty(lstSalida)) {
-			map.put("mensaje", "El empleado " + obj.getNombres()+ " " + obj.getApellidos() + " ya existe");
-			List<Empleado> lista = empleadoService.listaPorNombreApellidoLike("%");
-			map.put("lista", lista);
-			return map;
-		}
-				
+			
 		obj.setFechaRegistro(optEmpleado.get().getFechaRegistro());
 		obj.setEstado(optEmpleado.get().getEstado());
 		obj.setFechaActualizacion(new Date());
-		
-		//Para la PC2
-		//Usuario objUsuario=(Usuario)session.getAttribute("objUsuario");
-		//obj.setUsuarioActualiza(objUsuario);
 		
 		Empleado objSalida = empleadoService.actualizaEmpleado(obj);
 		if (objSalida == null) {
